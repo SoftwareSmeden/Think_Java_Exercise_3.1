a592ff6a11bedf1c2a015f507927ad1019357037
SoftwareSmeden <torb815g@edu.easj.dk>
2021-09-07 18:37:38 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int numberOne = 20;
+        double numberTwo = 20;
+        System.out.printf("%f", numberOne);
+        System.out.printf("%d", numberTwo);
+
+        /* Fejlen i "System.out.printf("%f", numberOne);", skyldes at "%f" bruges til at vise "double" værdier (se billede 1).
+           Fejlen i "System.out.printf("%d", numberTwo);", skyldes af "%d" bruges til at vise "Int" værdier (se billede 2). */
+    }
+}
